8d8c21f0114b818479b2c2a60007c2bfdb0e7b5c
unknown <simsoul1247@gmail.com>
2025-03-30 17:43:45 +0300

    second

---
+++ str.upper().py
@@ -0,0 +1,11 @@
+text = "gOoD MorNIng addis ABABA"
+uppercase_string = text.upper()
+lowercase_string = text.lower()
+title_string = text.title()
+print("Original text:", text)
+#str.upper converts all the given lowercase letters in a string to uppercase.
+print("Uppercase String:", uppercase_string)
+#str.lower converts all the given uppercase letters in a string to lowercase.
+print("lowercase string:", lowercase_string)
+#str.title converts 1st character of each words in a string to uppercase and the rest characters to lowercase.
+print("Title case string:", title_string)
